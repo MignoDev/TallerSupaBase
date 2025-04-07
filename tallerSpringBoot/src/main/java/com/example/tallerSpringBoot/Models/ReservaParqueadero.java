e9b997ef79a85d9c154cf67ef9d77dcb75d22ec2
MignoDev <miguelbejaranoing@gmail.com>
2025-04-07 15:46:19 -0500

    Creacion de modelos DetalleReservaParqueadero y ReservaParqueadero

---
+++ tallerSpringBoot/src/main/java/com/example/tallerSpringBoot/Models/ReservaParqueadero.java
@@ -0,0 +1,28 @@
+package com.example.tallerSpringBoot.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class ReservaParqueadero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idReserva;
+
+    private LocalDate fecha;
+
+    private LocalTime horaInicio;
+
+    private long idPropietario;
+}
